885bdecac787a337ac6dcabff5f8e4e81f6d6771
Mathias <mathias@skarving.com>
2026-03-06 14:55:04 +0100

    Implement BaseEntity class with component management methods

--- Common/src/main/java/io/donut/common/BaseEntity.java
+++ Common/src/main/java/io/donut/common/BaseEntity.java
@@ -1,4 +1,35 @@
 package io.donut.common;
 
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * The BaseEntity class that every entity must derive from.
+ */
 public abstract class BaseEntity {
+
+    private final Map<Class<? extends BaseComponent>, BaseComponent> componentMap = new HashMap<>();
+
+    /**
+     * Add a component to the entity if a component of the same type is not already present.
+     * @param component the component to add.
+     * @return boolean indicating successful insert.
+     */
+    public <T extends BaseComponent> boolean addComponent(T component) {
+        return componentMap.putIfAbsent(component.getClass(), component) != null;
+    }
+
+    /**
+     * Finds the component of type T if present, otherwise null.
+     * @param componentType the component type to get.
+     * @return the instance of the component type or null.
+     */
+    public <T extends BaseComponent> T getComponent(Class<T> componentType) {
+        if(!componentMap.containsKey(componentType)) return null;
+
+        BaseComponent component = componentMap.get(componentType);
+        if(!componentType.isAssignableFrom(component.getClass())) return null;
+
+        return componentType.cast(component);
+    }
 }
