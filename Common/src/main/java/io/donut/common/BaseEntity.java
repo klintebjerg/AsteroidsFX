f96483275061c38a69b58c3745e754e5eb00688a
Emil Jørgensen <emiljorgensen05@gmail.com>
2026-03-06 17:18:17 +0100

    add hasComponent method to BaseEntity and add missing doc in BaseEntity

--- Common/src/main/java/io/donut/common/BaseEntity.java
+++ Common/src/main/java/io/donut/common/BaseEntity.java
@@ -9,6 +9,10 @@ import java.util.Map;
 public abstract class BaseEntity {
 
     private boolean isAlive = true;
+
+    /// Map from the class of a component to the instance of the component.
+    /// <br>
+    /// This ensures each entity only can have one of each type of component.
     private final Map<Class<? extends BaseComponent>, BaseComponent> componentMap = new HashMap<>();
 
     /**
@@ -17,6 +21,7 @@ public abstract class BaseEntity {
      * @return boolean indicating successful insert.
      */
     public <T extends BaseComponent> boolean addComponent(T component) {
+        // only add if not already present, never overwrite
         return componentMap.putIfAbsent(component.getClass(), component) != null;
     }
 
@@ -26,12 +31,27 @@ public abstract class BaseEntity {
      * @return the instance of the component type or null.
      */
     public <T extends BaseComponent> T getComponent(Class<T> componentType) {
+
+        // if type not in map, return
         if(!componentMap.containsKey(componentType)) return null;
 
         BaseComponent component = componentMap.get(componentType);
+
+        // component.getClass() returns the runtime class of the component, e.g. PositionComponent
+        // check if the class of component is equal to (or a subclass of) the componentType, if not, return
         if(!componentType.isAssignableFrom(component.getClass())) return null;
 
+        // cast the component to the type the caller requested
         return componentType.cast(component);
+    }
+
+    /**
+     * Checks if the entity has a component of the given type.
+     * @param componentType the component type to check for.
+     * @return true if the entity has a component of type, otherwise false
+     */
+    public <T extends BaseComponent> boolean hasComponent(Class<T> componentType){
+        return componentMap.containsKey(componentType);
     }
 
     /**
